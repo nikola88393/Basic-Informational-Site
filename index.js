dfafa7c8e1ada7a1579a704f8f2878019800cde1
Nikola <nikola88393@gmail.com>
2024-09-22 15:44:30 +0300

    Rewrite routing using express

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-
-  let path = "./";
-
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact-me":
-      path += "/contact-me.html";
-      break;
-    default:
-      path += "/404.html";
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-
-    res.write(data);
-    res.end();
-  });
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./contact-me.html", { root: __dirname });
+});
+app.use((req, res) => {
+  res.status(404).sendFile("./404.html", { root: __dirname });
 });
 
-server.listen(8080);
+app.listen(8080, () => {
+  console.log("listening to port 8080");
+});
